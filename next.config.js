74b165e6a7abd9061ee85cb25092f8e528d643a4
Farrukh Ahmed <92082052+Farrukh-Ahmed1@users.noreply.github.com>
2023-09-04 13:55:54 +0500

    added some config changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
-    serverComponents: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
@@ -23,9 +28,6 @@ const nextConfig = {
         hostname: "placehold.co",
       },
     ],
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
 };
 
